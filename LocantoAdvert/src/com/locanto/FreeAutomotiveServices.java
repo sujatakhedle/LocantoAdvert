b292686e07121e40eaa9e7e11f434997cac4c1df
sujatakhedle <sujatakhedle@gmail.com>
2016-10-09 12:54:11 +0530

    Update FreeAutomotiveServices.java

--- LocantoAdvert/src/com/locanto/FreeAutomotiveServices.java
+++ LocantoAdvert/src/com/locanto/FreeAutomotiveServices.java
@@ -5,13 +5,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class FreeAutomotiveServices {
+public class FreeAutomotiveServices
+{
 	
-	@FindBy(id="L_S")
-	private WebElement ServiceMenu;
+    @FindBy(id="L_S")
+    private WebElement ServiceMenu;
 	
-	@FindBy(id="L_S_516")
-	private WebElement AutomotiveService;
+    @FindBy(id="L_S_516")
+    private WebElement AutomotiveService;
 	
     @FindBy(xpath="(//a[contains(text(),'Proceed  ')])[1]")
     private WebElement ProceedButton;
@@ -20,7 +21,8 @@ public class FreeAutomotiveServices {
     {
     	PageFactory.initElements(driver, this);
     }
-    public void ClickAutomotiveServices(){
+    public void ClickAutomotiveServices()
+    {
     	ServiceMenu.click();
     	AutomotiveService.click();
     	ProceedButton.click();
