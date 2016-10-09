36a9e0e4257c0c2f74b4a0de420b200b1a7246e4
sujatakhedle <sujatakhedle@gmail.com>
2016-10-09 13:59:16 +0530

    Update AdPostEventService.java

--- LocantoAdvert/src/testng/AdPostEventService.java
+++ LocantoAdvert/src/testng/AdPostEventService.java
@@ -5,14 +5,13 @@ import com.locanto.AdvertiseServices;
 import com.locanto.BoostYourSuccess;
 import com.locanto.FreeEventServices;
 import com.locanto.LoginPage;
-
 import generic.ReadFromExcel;
 
-public class AdPostEventService extends BasePage {
-	
+public class AdPostEventService extends BasePage
+{
 	@Test
-	public void testAdPostEventService() throws Exception{
-		
+	public void testAdPostEventService() throws Exception
+	{
 		FreeEventServices ES= new FreeEventServices(driver);
 		ES.ClickEventService();
 		
@@ -22,24 +21,21 @@ public class AdPostEventService extends BasePage {
 		AdvertiseServices AD= new AdvertiseServices(driver);
 		String xlFile = "./excelfiles/Data.xlsx";
 		String xlSheet="AdForm";
-		//int rc=ReadFromExcel.getRowCount(xlFile, xlSheet);
 		int cc=ReadFromExcel.getCellCount(xlFile, xlSheet,0);
-		
 		for(int i=22; i<=22; i++)
 		{
-			String[] data= ReadFromExcel.readRow(xlFile, xlSheet, i ,cc);
+		String[] data= ReadFromExcel.readRow(xlFile, xlSheet, i ,cc);	
+		AD.enterData(data);
+		System.out.println("i value sys"+i);
+		
+		 BoostYourSuccess BS= new  BoostYourSuccess(driver);
+		 BS.ClickproceedButton();
 			
-			AD.enterData(data);
-			System.out.println("i value sys"+i);
-			
-			 BoostYourSuccess BS= new  BoostYourSuccess(driver);
-			 BS.ClickproceedButton();
-			
-			driver.navigate().back();
-			driver.navigate().back();
+		driver.navigate().back();
+		driver.navigate().back();
 		  
 		}
-}
+        }
 }		
 
 
