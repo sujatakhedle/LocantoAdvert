d897ed3fdcf2ff2bcc153760cce3c1e2647dae7a
sujatakhedle <sujatakhedle@gmail.com>
2016-10-09 14:05:54 +0530

    Update AdPostLegalServices.java

--- LocantoAdvert/src/testng/AdPostLegalServices.java
+++ LocantoAdvert/src/testng/AdPostLegalServices.java
@@ -9,10 +9,12 @@ import com.locanto.LoginPage;
 
 import generic.ReadFromExcel;
 
-public class AdPostLegalServices extends BasePage {
-@Test
+public class AdPostLegalServices extends BasePage
+{
+        @Test
 	
-	public void testAdPostLegalServices() throws Exception{
+	public void testAdPostLegalServices() throws Exception
+	{
 		FreeLegalServices LS=new FreeLegalServices(driver);
 		LS.ClickLegalService();
 		
@@ -22,27 +24,20 @@ public class AdPostLegalServices extends BasePage {
 		AdvertiseServices AD= new AdvertiseServices(driver);
 		String XlFile= "./excelfiles/Data.xlsx";
 		String XlSheet="AdForm";
-		//int rc=ReadFromExcel.getRowCount(XlFile, XlSheet);
 		int cc=ReadFromExcel.getCellCount(XlFile, XlSheet, 0);
-		
 		for(int i=23; i<=23; i++)
 		{
-			
-				String[] data=ReadFromExcel.readRow(XlFile, XlSheet, i, cc);
-			
-				AD.enterData(data);
+		String[] data=ReadFromExcel.readRow(XlFile, XlSheet, i, cc);
+		AD.enterData(data);
 				
-		         BoostYourSuccess BS=new BoostYourSuccess(driver);
-		         BS.ClickproceedButton();
+		BoostYourSuccess BS=new BoostYourSuccess(driver);
+	        BS.ClickproceedButton();
 		
-		         driver.navigate().back();
-		         driver.navigate().back();	
+	        driver.navigate().back();
+	        driver.navigate().back();	
 		         
-		        Reporter.log(data+"+",true);
-		       
-		        
-			}
-			
-		}
-  }
+	        Reporter.log(data+"+",true);        
+	        }	
+	 }
+}
 
