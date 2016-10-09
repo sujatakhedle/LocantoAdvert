5ebbf0b7bbe653941dc2a7873e9ac29f7bd856ae
sujatakhedle <sujatakhedle@gmail.com>
2016-10-09 12:52:53 +0530

    Update BoostYourSuccess.java

--- LocantoAdvert/src/com/locanto/BoostYourSuccess.java
+++ LocantoAdvert/src/com/locanto/BoostYourSuccess.java
@@ -5,16 +5,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class BoostYourSuccess {
+public class BoostYourSuccess
+{
 	
 	@FindBy(xpath="(//button[contains(text(),'Proceed  ')])[1]")
 	private WebElement proceedButton;
 	
-	public  BoostYourSuccess(WebDriver driver){
+	public  BoostYourSuccess(WebDriver driver)
+	{
 		PageFactory.initElements(driver, this);
 	}
 		
-		public void ClickproceedButton(){
+		public void ClickproceedButton()
+		{
 			proceedButton.click();
 		}
 }
