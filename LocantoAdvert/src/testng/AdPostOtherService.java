ac0bdc544dbd543dcf00fdcd03065295c94e677e
sujatakhedle <sujatakhedle@gmail.com>
2016-10-09 14:09:47 +0530

    Update AdPostOtherService.java

--- LocantoAdvert/src/testng/AdPostOtherService.java
+++ LocantoAdvert/src/testng/AdPostOtherService.java
@@ -5,48 +5,36 @@ import com.locanto.AdvertiseServices;
 import com.locanto.BoostYourSuccess;
 import com.locanto.FreeOtherServices;
 import com.locanto.LoginPage;
-
 import generic.ReadFromExcel;
 
-public class AdPostOtherService extends BasePage {
-	
-@Test
+public class AdPostOtherService extends BasePage 
+{	
+    @Test
 
-public void testAdPostOtherService() throws Exception{
-	FreeOtherServices OS= new FreeOtherServices(driver);
-	OS.ClickOtherService();
+    public void testAdPostOtherService() throws Exception
+    {
+	 FreeOtherServices OS= new FreeOtherServices(driver);
+	 OS.ClickOtherService();
 	
-	LoginPage LP=new LoginPage(driver);
-	LP.Login("dmamta419@gmail.com","myad@locanto");
+	 LoginPage LP=new LoginPage(driver);
+	 LP.Login("dmamta419@gmail.com","myad@locanto");
 	
-	AdvertiseServices AD=new AdvertiseServices(driver);
-	String XlFile="./excelfiles/Data.xlsx";
-	String XlSheet= "AdForm";
-	//int rc=ReadFromExcel.getRowCount(XlFile, XlSheet);
-	int cc=ReadFromExcel.getCellCount(XlFile, XlSheet, 0);
-	
-
-	
-	for(int i=10; i<=14; i++)
-	{
-
-			String[] data=ReadFromExcel.readRow(XlFile, XlSheet, i, cc);
-		
-			AD.enterData(data);
+	 AdvertiseServices AD=new AdvertiseServices(driver);
+	 String XlFile="./excelfiles/Data.xlsx";
+	 String XlSheet= "AdForm";
+	 int cc=ReadFromExcel.getCellCount(XlFile, XlSheet, 0);
+	 for(int i=10; i<=14; i++)
+	 {
+	  String[] data=ReadFromExcel.readRow(XlFile, XlSheet, i, cc);
+	  AD.enterData(data);
 			
-	         BoostYourSuccess BS=new BoostYourSuccess(driver);
-	         BS.ClickproceedButton();
+	  BoostYourSuccess BS=new BoostYourSuccess(driver);
+	  BS.ClickproceedButton();
 	
-	         driver.navigate().back();
-	         driver.navigate().back();	
-	         
-
-		
-	}
-			
-}
-	
-	
+	 driver.navigate().back();
+	 driver.navigate().back();	
+	 }			
+    }	
 }
 
 
