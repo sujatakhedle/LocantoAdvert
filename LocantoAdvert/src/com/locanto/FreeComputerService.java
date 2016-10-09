a6d12cde763aeddbf4ef4266e5ac2312197b31d8
sujatakhedle <sujatakhedle@gmail.com>
2016-10-09 12:55:46 +0530

    Update FreeComputerService.java

--- LocantoAdvert/src/com/locanto/FreeComputerService.java
+++ LocantoAdvert/src/com/locanto/FreeComputerService.java
@@ -5,17 +5,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class FreeComputerService {
+public class FreeComputerService 
+{
 	@FindBy(id="L_S")
 	private WebElement ServiceMenu;
 	
 	@FindBy(id="L_S_504")
-    private WebElement ComputerService;
+        private WebElement ComputerService;
 	
 	@FindBy(xpath="(//a[contains(text(),'Proceed  ')])[1]")
 	private WebElement ProceedButton;
 	
-	public  FreeComputerService(WebDriver driver){
+	public  FreeComputerService(WebDriver driver)
+	{
 		PageFactory.initElements(driver, this);
 	}
 	public void ClickComputerService()
