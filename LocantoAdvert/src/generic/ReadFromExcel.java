285c47561c093724aa9ffccb1f4876328dbaa706
sujatakhedle <sujatakhedle@gmail.com>
2016-10-09 13:11:18 +0530

    Update ReadFromExcel.java

--- LocantoAdvert/src/generic/ReadFromExcel.java
+++ LocantoAdvert/src/generic/ReadFromExcel.java
@@ -7,59 +7,66 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 
 
-public class ReadFromExcel {
+public class ReadFromExcel
+{
 	
-		public static String getCellValue(String xlPath,String Sheet,int r, int c)
-		{
-			String v="";
-			try{
-				
-				Workbook wb=WorkbookFactory.create(new FileInputStream(xlPath));
-				v=wb.getSheet(Sheet).getRow(r).getCell(c).toString();
-		       
-			}
-			catch(Exception e){
-				
-			}
-			return v;
+	public static String getCellValue(String xlPath,String Sheet,int r, int c)
+	{
+		String v="";
+		try
+		{		
+		Workbook wb=WorkbookFactory.create(new FileInputStream(xlPath));
+		v=wb.getSheet(Sheet).getRow(r).getCell(c).toString();       
 		}
-		public static int getRowCount(String xlPath,String Sheet){
-			int rc=0;
-			try{
-				Workbook wb=WorkbookFactory.create(new FileInputStream(xlPath));
-				rc=wb.getSheet(Sheet).getLastRowNum();
-			}
-			catch(Exception e)
-			{
-				
-			}
-			return rc;
-		}
-		
-		public static int getCellCount(String xlPath, String Sheet, int Row )
+		catch(Exception e)
 		{
-			
-		    int lastColumn=0;
-			try{
-			Workbook wb=WorkbookFactory.create(new FileInputStream(xlPath)); //Use XSSFWorkBook incase of error, resolved in apache poi 3.7
-		    Row rowNo=wb.getSheet(Sheet).getRow(Row);
-		    lastColumn=(int) rowNo.getLastCellNum(); //getLastCellNum return short type, upcasted to int type
-		    }
-			catch(Exception e){
 				
-			}
-			return lastColumn;
-
+		}
+		return v;
+	}
+	
+	public static int getRowCount(String xlPath,String Sheet)
+	{
+		int rc=0;
+		try
+		{
+		Workbook wb=WorkbookFactory.create(new FileInputStream(xlPath));
+		rc=wb.getSheet(Sheet).getLastRowNum();
+		}
+		catch(Exception e)
+		{
+				
+		}
+		return rc;
 	}
 		
-		public static String[] readRow(String xlPath,String xlSheet,int row, int columnCount)
+	public static int getCellCount(String xlPath, String Sheet, int Row )
+	{	
+		int lastColumn=0;
+		try
 		{
-			String[] data=new String[columnCount];
+		//Use XSSFWorkBook incase of error, resolved in apache poi 3.7	
+		Workbook wb=WorkbookFactory.create(new FileInputStream(xlPath)); 
+		Row rowNo=wb.getSheet(Sheet).getRow(Row);
+		//getLastCellNum return short type, upcasted to int type	
+		lastColumn=(int) rowNo.getLastCellNum(); 
+	        }
+		catch(Exception e)
+		{
+				
+		}
+		return lastColumn;
+	}
+		
+	public static String[] readRow(String xlPath,String xlSheet,int row, int columnCount)
+	{
+		String[] data=new String[columnCount];
 						
 	        for(int j=0;j<columnCount;j++)
 	        {
-	        	data[j]=ReadFromExcel.getCellValue(xlPath, xlSheet, row, j);
+	        data[j]=ReadFromExcel.getCellValue(xlPath, xlSheet, row, j);
 	        }
 	        return data;
-		}
 	}
+}
+
